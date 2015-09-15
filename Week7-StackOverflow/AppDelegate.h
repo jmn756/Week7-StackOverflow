f62c8b5ae949a4820916181ecb95ea252e5807cf
jmn756 <joeynessif@gmail.com>
2015-09-15 09:29:07 -0700

    Committing initial project files

---
+++ Week7-StackOverflow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week7-StackOverflow
+//
+//  Created by Joey Nessif on 9/15/15.
+//  Copyright (c) 2015 Joey Nessif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week7-StackOverflow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Week7-StackOverflow
+//
+//  Created by Joey Nessif on 9/15/15.
+//  Copyright (c) 2015 Joey Nessif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
